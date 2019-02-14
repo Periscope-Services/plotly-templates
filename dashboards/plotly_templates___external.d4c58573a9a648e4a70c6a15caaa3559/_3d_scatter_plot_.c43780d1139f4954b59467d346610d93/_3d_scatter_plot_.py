674b0e565c872373a8ca9fc3109156fd54e7446c
Periscope Data <periscope@periscopedata.com>
2019-02-14 18:35:33 +0000

    Robert Friedland created Chart: [3D Scatter Plot]

---
+++ dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/_3d_scatter_plot_.c43780d1139f4954b59467d346610d93/_3d_scatter_plot_.py
@@ -0,0 +1,25 @@
+import plotly.plotly as py
+import plotly.graph_objs as go
+import pandas as pd
+import numpy as np
+
+trace = go.Scatter3d(
+  x=df['x'],
+  y=df['y'],
+  z=df['z'],
+  mode='markers',
+  marker={
+    'opacity': 0.8
+  }
+)
+data=[trace]
+layout = go.Layout(
+  margin={
+    'l': 0,
+    'r': 0,
+    't': 0,
+    'b': 0
+  }
+)
+fig = go.Figure(data=data, layout=layout)
+periscope.plotly(fig)

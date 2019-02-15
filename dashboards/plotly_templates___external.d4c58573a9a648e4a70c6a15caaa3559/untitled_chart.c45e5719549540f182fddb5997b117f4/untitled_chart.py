e9574718d565309b1241d2d2fcee90ac2e7c40b2
Periscope Data <periscope@periscopedata.com>
2019-02-15 19:47:37 +0000

    Robert Friedland updated Chart: Untitled Chart

--- dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/untitled_chart.c45e5719549540f182fddb5997b117f4/untitled_chart.py
+++ dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/untitled_chart.c45e5719549540f182fddb5997b117f4/untitled_chart.py
@@ -3,8 +3,30 @@ import pandas as pd
 import plotly.plotly as py
 import plotly.graph_objs as go
 
+def rgb_from_hex(hex):
+  h = hex.lstrip('#')
+  return tuple(((int(h[i:i+2], 16))) for i in (0, 2 ,4))
+
+def rgb_to_hex(rgb):
+  return '#%02x%02x%02x' % rgb
+
+def gradient(value, scale, colors):
+  min = scale[0]
+  max = scale[1]
+  if value < min: 
+    value = min
+  elif value > max:
+    value = max
+  pct = 1.0 * (value - min) / (max - min)
+  min_color = rgb_from_hex(colors[0])
+  max_color = rgb_from_hex(colors[1])
+  rgb = tuple(((int(round(pct * (max_color[i] - min_color[i]) + min_color[i])))) 	for i in range(0,3))
+  return rgb_to_hex(rgb)
+
 def arc(offset, start, end):
-  return 1.0 * (end - start) / 100 * offset
+  pct =  1.0 * (end - start) / 100
+  color = gradient(pct, [0, 1], ['#ff0000', '#00be11'])
+  return pct * offset, color
 
 offset = 50
 

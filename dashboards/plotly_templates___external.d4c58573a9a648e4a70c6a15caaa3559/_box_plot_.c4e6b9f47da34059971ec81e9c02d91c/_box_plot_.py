c978b94450f4be0d733cfdf5e4878dd54a63da7a
Periscope Data <periscope@periscopedata.com>
2019-02-15 18:50:45 +0000

    Robert Friedland created Chart: [BOX PLOT]

---
+++ dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/_box_plot_.c4e6b9f47da34059971ec81e9c02d91c/_box_plot_.py
@@ -0,0 +1,37 @@
+import pandas as pd 
+import plotly.plotly as py
+import plotly.graph_objs as go
+import datetime
+from datetime import timedelta
+
+def column_name(column):
+  return column.split('_', 1)[1].replace('_',' ').title()
+
+def get_columns(df):
+  y_column = [c for c in df.columns if c.startswith('Y')][0]
+  series_columns = [c for c in df.columns if c.startswith('S')]
+  unique_series = unique_vals(df, series_columns) if len(series_columns) > 0 else None
+  return y_column, series_columns, unique_series
+
+def unique_vals(df, column):
+  return df.groupby(column).size().reset_index()[column]
+
+df.columns = [c.upper() for c in df.columns]
+y_column, series_columns, unique_series = get_columns(df)
+has_series = unique_series is not None
+# showlegend = has_series
+
+traces = []
+for idx, series in unique_series.iterrows():
+  query = ' & '.join(f'{col} == "{series[{col}].iloc[0]}"' for col in series_columns)
+  df_series = df.query(query)
+  traces.append(
+    go.Box(
+      y=df_series[y_column],
+      boxpoints = False,
+      name=f'{", ".join([series[{col}].iloc[0] for col in series_columns])}'
+    )
+  )
+
+data = traces
+periscope.plotly(data)

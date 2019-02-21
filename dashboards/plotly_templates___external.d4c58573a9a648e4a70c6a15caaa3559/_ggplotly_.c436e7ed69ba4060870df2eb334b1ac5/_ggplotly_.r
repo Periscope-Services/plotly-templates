864a0fb16e4ac04d57ba2957b4050d811d90e525
Periscope Data <periscope@periscopedata.com>
2019-02-21 20:53:09 +0000

    Robert Friedland created Chart: [ggplotly]

---
+++ dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/_ggplotly_.c436e7ed69ba4060870df2eb334b1ac5/_ggplotly_.r
@@ -0,0 +1,10 @@
+library(ggplot2) 
+library(plotly)
+
+# ggplot object, non-interactive
+p=ggplot(iris, aes(x=Sepal.Length, y=Sepal.Width, color=Species, shape=Species)) + 
+    geom_point(size=6, alpha=0.6)
+
+p
+# plotly object!
+periscope.plotly(ggplotly(p))

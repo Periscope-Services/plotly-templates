f478236a93a61294f69fcd2d73286632d4fc0bf8
Periscope Data <periscope@periscopedata.com>
2019-03-04 22:14:15 +0000

    Robert Friedland created Chart: [FUNNEL 2]

---
+++ dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/_funnel_2_.c4e1abff101e4f9a965e1c43f1233133/_funnel_2_.py
@@ -0,0 +1,5 @@
+# SQL output is imported as a dataframe variable called 'df'
+import pandas as pd 
+
+# Use Periscope to visualize a dataframe, text, or an image by passing data to periscope.table(), periscope.text(), or periscope.image() respectively.
+periscope.table(df)

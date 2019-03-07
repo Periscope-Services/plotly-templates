4e92815db099cb001c282e6ace57f606df15b485
Periscope Data <periscope@periscopedata.com>
2019-03-07 22:47:08 +0000

    Robert Friedland updated Chart: Channel Performance

--- dashboards/sales_and_marketing_code.d4033d94ad4d4b3585807c59214cd1fe/channel_performance.c4fd204b70c94b329cb48c604d2e90e3/channel_performance.sql
+++ dashboards/sales_and_marketing_code.d4033d94ad4d4b3585807c59214cd1fe/channel_performance.c4fd204b70c94b329cb48c604d2e90e3/channel_performance.sql
@@ -1,1 +1,16 @@
-select 1
+select
+  channel
+  , cost
+  , leads
+  , pipeline
+  , dense_rank() over(order by cost desc, channel) as cost_rank
+  , dense_rank() over(order by leads desc, channel) as leads_rank
+  , dense_rank() over(order by pipeline desc, channel) as pipeline_rank
+from
+  table
+order by
+  2 desc
+  , 3 desc
+  , 4 desc
+  , 1 asc
+limit 20

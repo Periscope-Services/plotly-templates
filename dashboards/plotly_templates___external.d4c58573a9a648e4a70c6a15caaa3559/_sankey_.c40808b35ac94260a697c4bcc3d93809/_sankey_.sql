e9199ec70f4b47683f15f802101431120f881b56
Periscope Data <periscope@periscopedata.com>
2019-03-15 02:53:53 +0000

    Robert Friedland updated Chart: [SANKEY]

--- dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/_sankey_.c40808b35ac94260a697c4bcc3d93809/_sankey_.sql
+++ dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/_sankey_.c40808b35ac94260a697c4bcc3d93809/_sankey_.sql
@@ -1,9 +1,40 @@
+with
+  u as (
+    select
+      id
+    from
+      periscope_views.users
+    where
+      source is not null
+      and platform is not null
+      and id % 1000 = 29
+  )
 select
-  unique_id
-  , stage_name
-  , sort
+  u.id as unique_id
+  , source as stage_name
+  , dateadd(day, -100, created_at) as sort
 from
-  [sankey_dummy_data]
-order by
-  unique_id
-  , sort
+  u
+  inner join periscope_views.users on
+    u.id = users.id
+union all
+select
+  u.id
+  , platform
+  , created_at
+from
+  u
+  inner join periscope_views.users on
+    u.id = users.id
+union all
+select
+  u.id
+  , 'Made a purchase'
+  , dateadd(day, 100, min(created_at))
+from
+  u
+  inner join periscope_views.purchases on
+    u.id = purchases.user_id
+group by
+  1
+  , 2

34097dd658c20ad0f0539fe6021aa23e77afd311
Periscope Data <periscope@periscopedata.com>
2019-03-15 18:13:27 +0000

    Robert Friedland updated Chart: [GANTT]

--- dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/_gantt_.c4a6035ce72f4e82bb1b5a9dd5591761/_gantt_.py
+++ dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/_gantt_.c4a6035ce72f4e82bb1b5a9dd5591761/_gantt_.py
@@ -3,11 +3,6 @@ import plotly.plotly as py
 import plotly.figure_factory as ff
 
 df.columns = [c.title() for c in df.columns]
-fig = ff.create_gantt(df, height=300, width=400)
-fig['layout']['margin'] = {
-  't': 80,
-  'b': 20,
-  'l': 60,
-  'r': 20
-}
+fig = ff.create_gantt(df)
+
 periscope.plotly(fig)

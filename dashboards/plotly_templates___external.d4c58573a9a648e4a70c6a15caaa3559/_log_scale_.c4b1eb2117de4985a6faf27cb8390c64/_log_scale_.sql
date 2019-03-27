9404877b192922edb3403831616f1749490df7cf
Periscope Data <periscope@periscopedata.com>
2019-03-27 21:00:49 +0000

    Robert Friedland created Chart: [LOG SCALE]

---
+++ dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/_log_scale_.c4b1eb2117de4985a6faf27cb8390c64/_log_scale_.sql
@@ -0,0 +1,31 @@
+select
+  'a' as x_day
+  , 8 as y_tickets
+union
+select
+  'b'
+  , 7
+union
+select
+  'c'
+  , 6
+union
+select
+  'd'
+  , 5
+union
+select
+  'e'
+  , 4
+union
+select
+  'f'
+  , 3
+union
+select
+  'g'
+  , 2
+union
+select
+  'h'
+  , 1

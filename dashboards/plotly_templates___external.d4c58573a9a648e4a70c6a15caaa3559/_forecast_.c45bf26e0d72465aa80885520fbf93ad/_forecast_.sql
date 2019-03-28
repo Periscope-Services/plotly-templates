4ede15200d7d9aab26060177861eaea8d68cf95b
Periscope Data <periscope@periscopedata.com>
2019-03-28 20:17:50 +0000

    Robert Friedland updated Chart: [FORECAST]

--- dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/_forecast_.c45bf26e0d72465aa80885520fbf93ad/_forecast_.sql
+++ dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/_forecast_.c45bf26e0d72465aa80885520fbf93ad/_forecast_.sql
@@ -4,7 +4,7 @@ SQL output should have 3 columns:
 
    2) y_value: the value to forecast. name it whatever makes sense, e.g. y_signups, y$_revenue, etc. add the dollar sign ($) to format in dollars.
 
-   3) aggregation: the level of aggregation. can reference the [aggregation] filter. allowable values: hour, day, week, month, quarter, year
+   3) aggregation: the level of date aggregation. can reference the [aggregation] filter. allowable values: hour, day, week, month, quarter, year
 *****************************************/
 
 select

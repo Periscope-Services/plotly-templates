9d39bd15ac20fd9df8ece58051151e229a4cb4d4
Periscope Data <periscope@periscopedata.com>
2019-03-28 19:27:37 +0000

    Robert Friedland updated Chart: Forecast

--- dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/forecast.c45bf26e0d72465aa80885520fbf93ad/forecast.py
+++ dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/forecast.c45bf26e0d72465aa80885520fbf93ad/forecast.py
@@ -4,9 +4,24 @@ from fbprophet import Prophet
 import plotly.plotly as py
 import plotly.graph_objs as go
 
+def aggregation(df):
+  return df['aggregation'].iloc[0]
+
 df.columns = [c.lower() for c in df.columns]
 df['y'] = pd.to_numeric(df['y'])
 
+m = Prophet()
+m.fit(df[['ds','y']])
+
+agg = aggregation(df)
+if agg = 'day':
+  future = m.make_future_dataframe(periods=365)
+elif agg = 'week':
+  future = m.make_future_dataframe(periods=365)
+elif agg = 'month':
+  future = m.make_future_dataframe(periods=12, freq='M')
+
+forecast = m.predict(future)
 
 
 # Use Periscope to visualize a dataframe, text, or an image by passing data to periscope.table(), periscope.text(), or periscope.image() respectively.

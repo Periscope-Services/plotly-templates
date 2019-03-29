f13609e20a64f85a2844aa1949ffeb055abde833
Periscope Data <periscope@periscopedata.com>
2019-03-29 18:01:06 +0000

    Robert Friedland updated Chart: [FORECAST]

--- dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/_forecast_.c45bf26e0d72465aa80885520fbf93ad/_forecast_.py
+++ dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/_forecast_.c45bf26e0d72465aa80885520fbf93ad/_forecast_.py
@@ -1,4 +1,4 @@
-# FORECAST TEMPLATE
+# PERISCOPE FORECAST TEMPLATE
 # SQL output should have 2 columns:
 #    1) ds_aggregation: the date or datetime. name should be ds_hour, ds_day, ds_week, ds_month, ds_quarter, or ds_year. if you are using Periscope's aggregation filter, you can name it ds_[aggregation]
 

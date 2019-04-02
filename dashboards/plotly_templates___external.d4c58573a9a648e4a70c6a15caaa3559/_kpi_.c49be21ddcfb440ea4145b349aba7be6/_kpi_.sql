34e3aba1374701a8367154551d3da533e7458652
Periscope Data <periscope@periscopedata.com>
2019-04-02 18:46:16 +0000

    Robert Friedland updated Chart: [KPI]

--- dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/_kpi_.c49be21ddcfb440ea4145b349aba7be6/_kpi_.sql
+++ dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/_kpi_.c49be21ddcfb440ea4145b349aba7be6/_kpi_.sql
@@ -1,11 +1,6 @@
-/***********************************
-SQL output should have two columns:
-  1) date
-  2) kpi -- this is the KPI value to display. if it's a dollar or percentage value, call it kpi_$ or kpi_% to apply formatting
-***********************************/
 select
-  [created_at:month] as date
-  , sum(price) as "kpi_$"
+  [created_at:month] as ds_month
+  , sum(price) as "y$_revenue"
 from
   periscope_views.purchases
 group by

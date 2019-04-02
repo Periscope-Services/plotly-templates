9ed7c301161eeb21a32bedcd5bb5b8cc078e973c
Periscope Data <periscope@periscopedata.com>
2019-04-02 17:01:18 +0000

    Robert Friedland updated Chart: [SPARKBAR]

--- dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/_sparkbar_.c43a2d25dd524d778f4b7e0fcf6994fc/_sparkbar_.py
+++ dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/_sparkbar_.c43a2d25dd524d778f4b7e0fcf6994fc/_sparkbar_.py
@@ -3,7 +3,15 @@ import plotly.plotly as py
 import plotly.graph_objs as go
 import datetime
 
-def format(column):
+def get_formatter(column):
+  if '$' in column:
+    return '$'
+  elif '%' in column:
+    return '%'
+  else:
+    return None
+
+def tickformat(column):
   if column.startswith('y$'):
     return '$.3s'
   elif column.startswith('y%'):
@@ -20,6 +28,30 @@ def aggregation(ds_col):
 def percent(pct):
   return str(int(round(pct*100))) + '%'
 
+def format(num, formatter = None):
+  if formatter is None:
+    return abbrev(num)
+  elif formatter == '$':
+    return dollars(num)
+  elif formatter == '%':
+    return percent(num)
+
+def dollars(num):
+  num = float('{:.3g}'.format(float(num)))
+  magnitude = 0
+  while abs(num) >= 1000:
+    magnitude += 1
+    num /= 1000.0
+  return '${}{}'.format('{:f}'.format(num).rstrip('0').rstrip('.'), ['', 'K', 'M', 'B', 'T'][magnitude])
+
+def abbrev(num):
+  num = float('{:.3g}'.format(float(num)))
+  magnitude = 0
+  while abs(num) >= 1000:
+    magnitude += 1
+    num /= 1000.0
+  return '{}{}'.format('{:f}'.format(num).rstrip('0').rstrip('.'), ['', 'K', 'M', 'B', 'T'][magnitude])
+
 df.columns = [c.lower() for c in df.columns]
 y_col = [c for c in df.columns if c.startswith('y')][0]
 ds_col = [c for c in df.columns if c.startswith('ds')][0]
@@ -29,18 +61,18 @@ pct_col = 'pct_change'
 df[pct_col] = df[y_col].pct_change()
 
 current = df.tail(1)
-current_val = current[y_col]
-current_pct = current[pct_col]
+current_val = current[y_col].iloc[0]
+current_pct = current[pct_col].iloc[0]
 
 direction = ''
 if current_pct > 0:
-  direction = 'up ' + percent(abs(current_pct)) + ' from last ' + agg
+  direction = ', up ' + percent(abs(current_pct)) + ' from last ' + agg
 elif current_pct < 0:
-  direction = 'down ' + percent(abs(current_pct)) + ' from last ' + agg
-else:
-  direction = '
-
-text = f'{column_name(y_col)} is {"up" if 
+  direction = ', down ' + percent(abs(current_pct)) + ' from last ' + agg
+  
+fmt = get_formatter(y_col)
+text = f'{column_name(y_col)} is {format(current_val, formatter=fmt)}{direction}.'
+print(text)
 big_text = go.Scatter(
   x = [0],
   y = [.15],

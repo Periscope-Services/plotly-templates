be6a8ee4524bae82844422ebb67ce3e5b693e1e4
Periscope Data <periscope@periscopedata.com>
2019-05-03 16:16:50 +0000

    Robert Friedland updated Chart: Untitled Chart

--- dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/untitled_chart.c44cc0806ea74a00bdd7bc41ec9075f8/untitled_chart.sql
+++ dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/untitled_chart.c44cc0806ea74a00bdd7bc41ec9075f8/untitled_chart.sql
@@ -1,7 +1,9 @@
 select
   platform as x_platform
-  , sum(price) as "current_$"
-  , sum(price) * 1.5 as "goal_$"
+  , sum(price) / 100000 as "current_$"
+  , sum(price) * 1.5 / 100000 as "goal_$"
+--   , 10 as current
+--   , 15 as goal
 from
   periscope_views.purchases
 group by

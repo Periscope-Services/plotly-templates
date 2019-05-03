68804217d0b39e2953df71874cca96010fc16282
Periscope Data <periscope@periscopedata.com>
2019-05-03 16:26:42 +0000

    Robert Friedland renamed Chart: [RACECAR PROGRESS TO GOAL]

--- dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/_racecar_progress_to_goal_.c44cc0806ea74a00bdd7bc41ec9075f8/_racecar_progress_to_goal_.sql
+++ dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/_racecar_progress_to_goal_.c44cc0806ea74a00bdd7bc41ec9075f8/_racecar_progress_to_goal_.sql
@@ -2,8 +2,6 @@ select
   platform as x_platform
   , sum(price) as "current_$"
   , sum(price) * 1.5 as "goal_$"
---   , 10 as current
---   , 15 as goal
 from
   periscope_views.purchases
 where [created_at=7days]

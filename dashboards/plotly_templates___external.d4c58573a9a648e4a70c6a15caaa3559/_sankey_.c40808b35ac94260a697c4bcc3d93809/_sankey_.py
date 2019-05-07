4da84468bdaf01a0ba3876a87feb020e397501bb
Periscope Data <periscope@periscopedata.com>
2019-05-07 17:45:38 +0000

    Robert Friedland updated Chart: [SANKEY]

--- dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/_sankey_.c40808b35ac94260a697c4bcc3d93809/_sankey_.py
+++ dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/_sankey_.c40808b35ac94260a697c4bcc3d93809/_sankey_.py
@@ -22,6 +22,8 @@ def rgb_from_hex(hex):
   h = hex.lstrip('#')
   return ",".join(str((int(h[i:i+2], 16))) for i in (0, 2 ,4))
 
+# identifies any circular references in the data
+# e.g. some items go from stage A -> B, some go from B -> A. this breaks the diagram
 def is_circular(df_agg, row):
   circular_reference = df_agg.query(f'prior_stage_index == {row["stage_index"]} & stage_index == {row["prior_stage_index"]}')
   if circular_reference.size == 0:
@@ -33,28 +35,34 @@ def is_circular(df_agg, row):
 
 df.columns = [c.lower() for c in df.columns]
 
+# get unique stages and assign indexes
 stages = df.groupby(['stage_name']).size().reset_index()[['stage_name']]
 stages['stage_index'] = stages.index
 stages['color'] = stages.apply(lambda r: color(r['stage_index']), axis=1)
 
-# print(stages)
+# identify the prior stage for each record
 df['stage_index'] = df.apply(lambda r: stages.loc[stages['stage_name'] == r['stage_name']]['stage_index'].iloc[0], axis=1)
 df['prior_stage_index'] = df.groupby('unique_id').stage_index.shift()
 
+# only take records that have a prior stage (can't plot something on a sankey if it doesn't go anywhere)
 df = df[np.isfinite(df['prior_stage_index'])]
 
+# get prior stage name
 df['prior_stage_name'] = df.apply(lambda r: stages.loc[stages['stage_index'] == r['prior_stage_index']]['stage_name'].iloc[0], axis=1)
-
 df['prior_stage_index'] = df['prior_stage_index'].astype(int)
 
+# aggregate counts
 df_agg = df.groupby(['prior_stage_index', 'prior_stage_name', 'stage_index', 'stage_name']).size().reset_index(name='count')
 
+# remove any circular references
 df_agg['is_circular'] = df_agg.apply(lambda row: is_circular(df_agg, row), axis=1)
 df_agg = df_agg.query('is_circular == False')
 
+# identifies the end stages in the sankey
 priors = df_agg['prior_stage_index']
 ends = stages.query('stage_index not in @priors')
 
+# apply colors to each flow
 df_agg['color'] = df_agg.apply(lambda r: f'rgba({rgb_from_hex(color(r["prior_stage_index"]))},{.25 if r["stage_index"] in ends["stage_index"] else .25})', axis=1)
     
 data=dict(

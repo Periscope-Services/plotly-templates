d1629dad50bfdd7a87ddaf73de8b9b594c879b8f
Periscope Data <periscope@periscopedata.com>
2019-05-07 17:44:33 +0000

    Ryan Waters updated Chart: [GANTT]

--- dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/_gantt_.c4a6035ce72f4e82bb1b5a9dd5591761/_gantt_.py
+++ dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/_gantt_.c4a6035ce72f4e82bb1b5a9dd5591761/_gantt_.py
@@ -2,7 +2,10 @@ import pandas as pd
 import plotly.plotly as py
 import plotly.figure_factory as ff
 
+# Create and assign list of column names
 df.columns = [c.title() for c in df.columns]
+
+# Use figure factory to create gantt chart
 fig = ff.create_gantt(df)
 
 periscope.plotly(fig)

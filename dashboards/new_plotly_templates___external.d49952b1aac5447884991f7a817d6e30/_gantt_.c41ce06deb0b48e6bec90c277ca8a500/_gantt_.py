40846fefc7c5057b98c49dda7bc809cf41c50fe7
Periscope Data <periscope@periscopedata.com>
2019-06-25 22:34:55 +0000

    Neha Kumar updated Chart: [GANTT]

--- dashboards/new_plotly_templates___external.d49952b1aac5447884991f7a817d6e30/_gantt_.c41ce06deb0b48e6bec90c277ca8a500/_gantt_.py
+++ dashboards/new_plotly_templates___external.d49952b1aac5447884991f7a817d6e30/_gantt_.c41ce06deb0b48e6bec90c277ca8a500/_gantt_.py
@@ -1,17 +1,27 @@
+# PERISCOPE GANTT CHART TEMPLATE
+# SQL output should have 3 columns:
+#    1) task: the name of the task
+#    2) start: a date indicating when the task has started or is scheduled to start
+#    2) finish: a date indicating when the task has ended or is scheduled to end
+
+# Import Libraries
 import pandas as pd
 import plotly.plotly as py
 import plotly.figure_factory as ff
 
+# Create dummy data
 community_post = 'https://community.periscopedata.com/t/63s966'
 dummy_df = pd.DataFrame()
 dummy_df['task'] = ['Task 1', 'Task 2', 'Task 3']
 dummy_df['start'] = ['2019-02-07', '2018-12-24', '2018-11-09']
 dummy_df['finish'] = ['2019-03-08', '2019-05-08', '2019-04-08']
 
+# HELPER FUNCTIONS
 def style_link(text, link, **settings):
   style = ';'.join([f'{key.replace("_","-")}:{settings[key]}' for key in settings])
   return f'<a href="{link}" style="{style}">{text}</a>'
 
+# MAIN FUNCTION
 def plot(df, annotation=None):
   # Create and assign list of column names
   df.columns = [c.title() for c in df.columns]
@@ -21,6 +31,7 @@ def plot(df, annotation=None):
   if annotation is not None:
     fig['layout']['annotations'] = [annotation]
   periscope.plotly(fig)
+
   
 try:
   plot(df)

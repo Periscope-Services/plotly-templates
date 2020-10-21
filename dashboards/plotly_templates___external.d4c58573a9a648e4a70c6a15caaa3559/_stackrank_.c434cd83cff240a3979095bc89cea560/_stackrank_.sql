8588be5dd3729e90e5e9541e592ec1a3d0aa8a47
Periscope Data <periscope@periscopedata.com>
2020-10-21 17:29:01 +0000

    Robert Friedland updated Chart: [STACKRANK]

--- dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/_stackrank_.c434cd83cff240a3979095bc89cea560/_stackrank_.sql
+++ dashboards/plotly_templates___external.d4c58573a9a648e4a70c6a15caaa3559/_stackrank_.c434cd83cff240a3979095bc89cea560/_stackrank_.sql
@@ -1,8 +1,25 @@
+with
+  dates as (
+    select
+      distinct date
+    from
+      [stackrank_dummy_data]
+    order by
+      1 desc
+    limit 7
+  )
 select
   date
   , cookie as entity
   , rank
 from
   [stackrank_dummy_data]
+where
+  date in (
+    select
+      date
+    from
+      dates
+  )
 order by
   1
